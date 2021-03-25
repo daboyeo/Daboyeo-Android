6a673b2518c805b9100bc73819509f7b1459f410
minhee <jwmh15@gmail.com>
2021-03-25 22:53:58 +0900

    Create SearchFragment

---
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/home/ui/SearchFragment.kt
@@ -0,0 +1,24 @@
+package com.example.daboyeo_android.home.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.daboyeo_android.R
+import com.example.daboyeo_android.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+
+        return binding.root
+    }
+
+}
