1cc446bd3f8845757d313ab35b38472cbbe37d61
minhee <jwmh15@gmail.com>
2021-03-26 22:56:12 +0900

    Create Login

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/sign/ui/LoginActivity.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/sign/ui/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.daboyeo_android
+package com.example.daboyeo_android.sign.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.daboyeo_android.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/sign/ui/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.daboyeo_android.sign.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.daboyeo_android.R
+import com.example.daboyeo_android.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
