5a61d47e9470e77174c5f740aca1dd2d917de03b
minhee <jwmh15@gmail.com>
2021-03-29 20:24:10 +0900

    Create detail Post

---
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/home/ui/DetailPostFragment.kt
@@ -0,0 +1,24 @@
+package com.example.daboyeo_android.home.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.daboyeo_android.R
+import com.example.daboyeo_android.databinding.FragmentDetailPostBinding
+
+class DetailPostFragment : Fragment() {
+    private lateinit var binding: FragmentDetailPostBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail_post, container, false)
+
+        return binding.root
+    }
+
+}
