10370d8d49a544f99443c60474246212ebbde630
minhee <jwmh15@gmail.com>
2021-04-01 22:14:18 +0900

    [Add] Google Map

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/writing/ui/LocationActivity.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/writing/ui/LocationActivity.kt
@@ -1,37 +1,70 @@
 package com.example.daboyeo_android.writing.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.daboyeo_android.R
 import com.example.daboyeo_android.databinding.ActivityLocationBinding
-import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
 
-class LocationActivity : AppCompatActivity() , OnMapReadyCallback{
-    private lateinit var binding : ActivityLocationBinding
+
+class LocationActivity : AppCompatActivity(), OnMapReadyCallback,
+    GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+    private lateinit var binding: ActivityLocationBinding
     lateinit var mapFragment: SupportMapFragment
-    lateinit var googleMap : GoogleMap
+    private lateinit var mMap: GoogleMap
+    private var lat = 0
+    private var lon = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_location)
         binding.location = this
 
-        mapFragment = supportFragmentManager.findFragmentById(R.id.location_mapView_fragment) as SupportMapFragment
+        mapFragment =
+            supportFragmentManager.findFragmentById(R.id.location_mapView_fragment) as SupportMapFragment
         mapFragment.getMapAsync(OnMapReadyCallback {
-            googleMap = it
+            mMap = it
             //googleMap.isMyLocationEnabled = true
 
         })
 
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
-        TODO("Not yet implemented")
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap ?: return
+        googleMap.setOnMyLocationButtonClickListener(this)
+        googleMap.setOnMyLocationClickListener(this)
     }
 
+    private fun locationZoom() {
+        val manager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        try {
+
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG).show()
+    }
 
 }
