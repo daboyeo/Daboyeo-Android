d2d7cad862951bf58f1e41168ed4795711664d6e
minhee <jwmh15@gmail.com>
2021-04-11 23:08:49 +0900

    [Add] API

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/network/DaboyeoAPI.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/network/DaboyeoAPI.kt
@@ -1,4 +1,56 @@
 package com.example.daboyeo_android.network
 
+import com.example.daboyeo_android.home.model.ReportData
+import com.example.daboyeo_android.profile.model.MyProfileData
+import retrofit2.Response
+import retrofit2.http.*
+
 interface DaboyeoAPI {
+
+    @POST("/auth")
+    suspend fun signAuth(
+            @Body body: HashMap<String, String>
+    ): Response<String>
+
+    @GET("/user")
+    suspend fun getMyProfile(
+            @Header("Authorization") header: String
+    ): Response<MyProfileData>
+
+    @PUT("/user")
+    suspend fun modifyProfile(
+            @Header("Authorization") header: String,
+            @Body body: HashMap<String, String>
+    ): Response<Unit>
+
+    @GET("/report")
+    suspend fun getTimeLine(
+            @Header("Authorization") header: String
+    ): Response<List<ReportData>>
+
+    @GET("/report/{id}")
+    suspend fun detailReport(
+            @Header("Authorization") header: String
+    )
+
+    @POST("/comment")
+    suspend fun comment(
+            @Header("Authorization") header: String,
+            @Body body: HashMap<String, String>
+    ): Response<Unit>
+
+    @PUT("/sympathy")
+    suspend fun sympathy(
+            @Header("Authorization") header: String,
+            @Body report_id: String
+    ): Response<Boolean>
+
+    @POST("/report")
+    suspend fun writing(
+            @Header("Authorization") header: String,
+            @Body tags: List<String>,
+            @Body image_uris: List<String>,
+            @Body content: String,
+            @Body location: String
+    )
 }
