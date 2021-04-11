25779f08868cc20b1a7696d4ee7baa3e07b10fbd
minhee <jwmh15@gmail.com>
2021-04-11 23:08:13 +0900

    [Add] method

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/home/ui/adapter/ReportsAdapter.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/home/ui/adapter/ReportsAdapter.kt
@@ -1,5 +1,20 @@
 package com.example.daboyeo_android.home.ui.adapter
 
-class PostAdapter(private ){
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.daboyeo_android.R
+import com.example.daboyeo_android.home.model.ReportData
+
+class ReportsAdapter(private val reports: List<ReportData>): RecyclerView.Adapter<ReportsViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReportsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_report, parent, false)
+        return ReportsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ReportsViewHolder, position: Int) = holder.bind(reports[position])
+
+    override fun getItemCount(): Int = reports.size
 
 }
