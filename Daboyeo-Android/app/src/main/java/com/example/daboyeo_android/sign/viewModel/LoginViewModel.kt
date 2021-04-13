4ee9e320dee353ee4cba50aa631b91413f9e084e
minhee <jwmh15@gmail.com>
2021-04-13 22:43:46 +0900

    [Delete] ViewModel

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/sign/viewModel/LoginViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.daboyeo_android.sign.viewModel
-
-import androidx.hilt.lifecycle.ViewModelInject
-import com.example.daboyeo_android.base.BaseViewModel
-import com.example.daboyeo_android.sign.model.LoginRepository
-
-class LoginViewModel @ViewModelInject constructor(val repository: LoginRepository) : BaseViewModel(){
-}
--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/writing/viewModel/WritingViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.daboyeo_android.writing.viewModel
-
-import androidx.hilt.lifecycle.ViewModelInject
-import com.example.daboyeo_android.base.BaseViewModel
-import com.example.daboyeo_android.writing.repository.WritingRepository
-
-class WritingViewModel @ViewModelInject constructor(val repository: WritingRepository) : BaseViewModel() {
-
-}
