538206d02ac6990d02ab3d9465545a43de9f884a
minhee <jwmh15@gmail.com>
2021-04-13 22:56:14 +0900

    [Create] ViewModel

---
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/home/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.daboyeo_android.ui.home
+
+import androidx.lifecycle.ViewModel
+import com.example.daboyeo_android.repository.HomeRepository
+
+class HomeViewModel constructor(val repository: HomeRepository): ViewModel() {
+
+}
---
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/writing/WritingViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.daboyeo_android.ui.writing
+
+import androidx.lifecycle.ViewModel
+import com.example.daboyeo_android.repository.WritingRepository
+
+class WritingViewModel constructor(val repository: WritingRepository) : ViewModel() {
+
+}
