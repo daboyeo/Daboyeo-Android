b59af5566ffc74d10f2555b31b94602b7a56c5a8
minhee <jwmh15@gmail.com>
2021-04-13 22:46:54 +0900

    [Crerate] ProfileViewModel

---
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.daboyeo_android.ui.profile
+
+import androidx.lifecycle.ViewModel
+import com.example.daboyeo_android.repository.ProfileRepository
+
+class ProfileViewModel constructor(val repository: ProfileRepository): ViewModel() {
+
+}
