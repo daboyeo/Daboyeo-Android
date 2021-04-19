f12c8eeefbf723b756c2cc27d75466ca26f3e014
minhee <jwmh15@gmail.com>
2021-04-19 22:45:33 +0900

    [Create] ReportData

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/entity/home/ReportData.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/entity/home/ReportData.kt
@@ -1,17 +1,15 @@
 package com.example.daboyeo_android.entity.home
 
-import com.google.gson.annotations.SerializedName
-
 data class ReportData(
-        @SerializedName("report_id") val report_id: String,
-        @SerializedName("reporter_name")  val reporter_name: String,
-        @SerializedName("reporter_profile_uri") val reporter_profile_uri: String,
-        @SerializedName("content") val content: String,
-        @SerializedName("tags") val tags: List<String>,
-        @SerializedName("image_uris") val image_uris: List<String>,
-        @SerializedName("num_of_sympathy") val num_of_sympathy : Int,
-        @SerializedName("is_sympathy") val is_sympathy: Boolean,
-        @SerializedName("created_at") val created_at: String,
-        @SerializedName("updated_at") val updated_at: String,
-        @SerializedName("location") val location: String
+        val report_id: String,
+        val reporter_name: String,
+        val reporter_profile_uri: String,
+        val content: String,
+        val tags: List<String>,
+        val image_uris: List<String>,
+        val num_of_sympathy: Int,
+        val is_sympathy: Boolean,
+        val created_at: String,
+        val updated_at: String,
+        val location: String
 )
