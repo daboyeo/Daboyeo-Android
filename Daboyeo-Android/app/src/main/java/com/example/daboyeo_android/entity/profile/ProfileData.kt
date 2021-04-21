545a8b6270e0ea67661b469a8c55f915fd205f8a
minhee <jwmh15@gmail.com>
2021-04-21 22:31:58 +0900

    [Rename] MyProfileData -> ProfileData

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/entity/profile/ProfileData.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/entity/profile/ProfileData.kt
@@ -2,7 +2,7 @@ package com.example.daboyeo_android.entity.profile
 
 import com.google.gson.annotations.SerializedName
 
-data class MyProfileData (
+data class ProfileData (
         @SerializedName("name") val name: String,
         @SerializedName("profile_uri") val profile_uri: String
         )
