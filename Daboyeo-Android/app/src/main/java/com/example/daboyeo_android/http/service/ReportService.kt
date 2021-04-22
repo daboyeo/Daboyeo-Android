9863482bd72401b63cf7fef12d2afc6e4cb2ce59
minhee <jwmh15@gmail.com>
2021-04-22 22:12:42 +0900

    [Create] ReportService

---
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/service/ReportService.kt
@@ -0,0 +1,36 @@
+package com.example.daboyeo_android.http.service
+
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ReportService {
+    @POST("/comment")
+    suspend fun comment(
+        @Header("Authorization") header: String,
+        @Body body: HashMap<String, String>
+    ): Response<Unit>
+
+    @PUT("/sympathy")
+    suspend fun sympathy(
+        @Header("Authorization") header: String,
+        @Body report_id: String
+    ): Response<Boolean>
+
+    @PUT("/report")
+    suspend fun modifyReport (
+        @Header("Authorization") header: String,
+        @Body body: HashMap<String, String>
+    ): Response<Unit>
+
+    @DELETE("/report")
+    suspend fun deleteReport (
+        @Header("Authorization") header: String,
+        @Body body: HashMap<String, String>
+    ): Response<Unit>
+
+    @DELETE("/comment")
+    suspend fun deleteComment (
+        @Header ("Authorization") header: String,
+        @Body body: HashMap<String, String>
+    ) : Response<Unit>
+}
