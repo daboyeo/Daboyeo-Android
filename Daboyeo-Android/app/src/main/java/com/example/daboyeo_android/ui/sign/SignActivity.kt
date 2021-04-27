0b581ef4ae6e0691713d70417ae4c694d13bc96f
minhee <jwmh15@gmail.com>
2021-04-27 22:06:51 +0900

    [Add] view transaction

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/sign/SignActivity.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/sign/SignActivity.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import com.example.daboyeo_android.R
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.sign_fragment, SignFragment())
+        fragmentTransaction.commit()
     }
 }
