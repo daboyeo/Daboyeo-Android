b2caec0803d277e49419c1565dcea09df132840c
minhee <jwmh15@gmail.com>
2021-04-27 22:03:57 +0900

    [Delete] comment, sympathy

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/service/TimelineService.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/service/TimelineService.kt
@@ -2,14 +2,16 @@ package com.example.daboyeo_android.http.service
 
 import com.example.daboyeo_android.entity.home.DetailReportData
 import com.example.daboyeo_android.entity.home.ReportData
-import com.example.daboyeo_android.entity.profile.MyProfileData
 import retrofit2.Response
 import retrofit2.http.*
 
-interface TimelineService : DaboyeoService {
+interface TimelineService {
     @GET("/report")
     suspend fun getTimeLine(
-            @Header("Authorization") header: String
+            @Header("Authorization") header: String,
+            @Query("user") user: String,
+            @Query("search") search: String,
+            @Query("tag") tag: String
     ): Response<List<ReportData>>
 
     @GET("/report/{id}")
@@ -17,15 +19,4 @@ interface TimelineService : DaboyeoService {
             @Header("Authorization") header: String
     ): Response<DetailReportData>
 
-    @POST("/comment")
-    suspend fun comment(
-            @Header("Authorization") header: String,
-            @Body body: HashMap<String, String>
-    ): Response<Unit>
-
-    @PUT("/sympathy")
-    suspend fun sympathy(
-            @Header("Authorization") header: String,
-            @Body report_id: String
-    ): Response<Boolean>
 }
