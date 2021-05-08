7df0cb044cfe220a577066f7398543150249bb35
minhee <jwmh15@gmail.com>
2021-05-08 23:04:20 +0900

    [Add] file server

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/interceptor/DaboyeoConnect.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/interceptor/DaboyeoConnect.kt
@@ -1,6 +1,7 @@
 package com.example.daboyeo_android.http.interceptor
 
 import com.example.daboyeo_android.http.service.DaboyeoService
+import com.example.daboyeo_android.http.service.FileService
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
@@ -9,13 +10,13 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-@Module
-@InstallIn(ActivityComponent::class)
 object DaboyeoConnect {
     private var retrofit: Retrofit
+    private var fileRetrofit: Retrofit
     private var api: DaboyeoService
+    private var fileApi : FileService
     private const val baseURL = "https://6l8nidp3k7.execute-api.ap-northeast-2.amazonaws.com/dev/"
-    const val s3 = ""
+    private const val fileServer = "http://54.180.105.66:8000/"
 
     init {
         val loggingInterceptor = HttpLoggingInterceptor()
@@ -31,8 +32,17 @@ object DaboyeoConnect {
             .client(client)
             .build()
 
+        fileRetrofit = Retrofit.Builder()
+            .baseUrl(fileServer)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
         api = retrofit.create(DaboyeoService::class.java)
+        fileApi = fileRetrofit.create(FileService::class.java)
     }
 
     fun getAPI() = api
+
+    fun getFileAPI() = fileApi
 }
