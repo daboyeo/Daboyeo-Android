defcc7bbfdce14b236e2ff86e039b4bda8523a20
minhee <jwmh15@gmail.com>
2021-05-08 23:04:05 +0900

    [Create] FileService

---
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/service/FileService.kt
@@ -0,0 +1,18 @@
+package com.example.daboyeo_android.http.service
+
+import okhttp3.MultipartBody
+import retrofit2.Response
+import retrofit2.http.*
+
+interface FileService {
+    @GET("/file")
+    suspend fun getFile(
+        @Query("uuid") uuid: String
+    ): Response<Unit>
+
+    @Multipart
+    @POST("/file")
+    suspend fun uploadFile(
+        @Part file : List<MultipartBody.Part>
+    ) : Response<Int>
+}
