2497a5c43d34578c975c6b989b65e7c9ef51f253
minhee <jwmh15@gmail.com>
2021-05-08 23:06:22 +0900

    [Modify] Daboyea -> Daboyeo

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/repository/ReportRepository.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/repository/ReportRepository.kt
@@ -1,21 +1,21 @@
 package com.example.daboyeo_android.repository
 
 import com.example.daboyeo_android.base.BaseRepository
-import com.example.daboyeo_android.http.interceptor.DaboyeaConnect
+import com.example.daboyeo_android.http.interceptor.DaboyeoConnect
 import com.example.daboyeo_android.http.interceptor.Result
 
 class ReportRepository : BaseRepository() {
     private val token = getAccessToken()!!
 
     suspend fun addComment(hashMap: HashMap<String, String>) : Result<Unit> {
-        return mappingToResult { DaboyeaConnect.getAPI().addComment(token, hashMap) }
+        return mappingToResult { DaboyeoConnect.getAPI().addComment(token, hashMap) }
     }
 
     suspend fun deleteComment(hashMap: HashMap<String, String>) : Result<Unit> {
-        return mappingToResult { DaboyeaConnect.getAPI().deleteComment(token, hashMap) }
+        return mappingToResult { DaboyeoConnect.getAPI().deleteComment(token, hashMap) }
     }
 
     suspend fun modifyReport(hashMap: HashMap<String, String>) : Result<Unit> {
-        return mappingToResult { DaboyeaConnect.getAPI().modifyReport(token, hashMap) }
+        return mappingToResult { DaboyeoConnect.getAPI().modifyReport(token, hashMap) }
     }
 }
--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/repository/SignRepository.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/repository/SignRepository.kt
@@ -1,12 +1,12 @@
 package com.example.daboyeo_android.repository
 
 import com.example.daboyeo_android.base.BaseRepository
-import com.example.daboyeo_android.http.interceptor.DaboyeaConnect
+import com.example.daboyeo_android.http.interceptor.DaboyeoConnect
 import com.example.daboyeo_android.http.interceptor.Result
 import java.util.*
 
 class SignRepository : BaseRepository(){
     suspend fun signAuth(hashMap: HashMap<String, String>) : Result<String> {
-        return mappingToResult { DaboyeaConnect.getAPI().signAuth(hashMap) }
+        return mappingToResult { DaboyeoConnect.getAPI().signAuth(hashMap) }
     }
 }
