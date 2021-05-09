727e6c6d68c6ade255c61b32dfe9b3794109392e
minhee <jwmh15@gmail.com>
2021-05-09 21:44:08 +0900

    [Rename] Daboyea -> Daboyeo

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/repository/ProfileRepository.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/repository/ProfileRepository.kt
@@ -3,25 +3,25 @@ package com.example.daboyeo_android.repository
 import com.example.daboyeo_android.base.BaseRepository
 import com.example.daboyeo_android.entity.home.ReportListData
 import com.example.daboyeo_android.entity.profile.ProfileData
-import com.example.daboyeo_android.http.interceptor.DaboyeaConnect
+import com.example.daboyeo_android.http.interceptor.DaboyeoConnect
 import com.example.daboyeo_android.http.interceptor.Result
 
 class ProfileRepository : BaseRepository(){
-    private val token = getAccessToken()!!
+    private val token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYyMDI3NjIyNiwianRpIjoiZGUxYjQ3ZDItNmZmZi00MzNkLTkxNzMtYzU2M2M2NmQwZmI4IiwibmJmIjoxNjIwMjc2MjI2LCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoiZ29vZ2xlQDExNDAyMTg5OTU3NTA5Nzc4NTU2MSIsImV4cCI6MTYyMTQ4NTgyNn0.9T7bu48-VW-fmOcjEPpDjoMwHYd-6TMwTYpxto5HBd8"
 
     suspend fun getMyProfile() : Result<ProfileData> {
-        return mappingToResult { DaboyeaConnect.getAPI().getMyProfile(token) }
+        return mappingToResult { DaboyeoConnect.getAPI().getMyProfile(token) }
     }
 
     suspend fun getUserProfile(userName : String) : Result<ProfileData> {
-        return mappingToResult { DaboyeaConnect.getAPI().getUserProfile(userName) }
+        return mappingToResult { DaboyeoConnect.getAPI().getUserProfile(userName) }
     }
 
     suspend fun getProfileReports(userName: String) : Result<ReportListData> {
-        return mappingToResult { DaboyeaConnect.getAPI().getProfileReports(token, userName) }
+        return mappingToResult { DaboyeoConnect.getAPI().getProfileReports(token, userName) }
     }
 
     suspend fun modifyProfile(hashMap: HashMap<String, String>) : Result<Unit> {
-        return mappingToResult { DaboyeaConnect.getAPI().modifyProfile(token, hashMap) }
+        return mappingToResult { DaboyeoConnect.getAPI().modifyProfile(token, hashMap) }
     }
 }
