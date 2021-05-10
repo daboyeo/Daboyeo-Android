d2b9ac2bfc689132df71e13a21a4e73d6ae090a2
minhee <jwmh15@gmail.com>
2021-05-10 22:41:38 +0900

    [Add] getDetailReport

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/report/ReportViewModel.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/report/ReportViewModel.kt
@@ -1,16 +1,38 @@
 package com.example.daboyeo_android.ui.report
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.daboyeo_android.entity.home.DetailReportData
 import com.example.daboyeo_android.repository.ReportRepository
 import kotlinx.coroutines.launch
+import com.example.daboyeo_android.http.interceptor.Result
 
 class ReportViewModel : ViewModel() {
 
     private val repository = ReportRepository()
 
-    fun clickSympathy() {
+    private val _detailReport = MutableLiveData<DetailReportData>()
+    val detailReport: LiveData<DetailReportData> get() = _detailReport
 
+    fun getDetailReport(reportId: Int) {
+        viewModelScope.launch {
+            when(val result = repository.getDetailReport(reportId)) {
+                is Result.Success -> {
+                    if(result.code == 200) {
+                        _detailReport.value = result.data
+                    }
+                }
+                is Result.Error -> {
+
+                }
+            }
+        }
+    }
+
+    fun clickSympathy(reportId: Int) {
+        //repository.clickSympathy()
     }
 
     fun modifyReport() {
@@ -26,7 +48,9 @@ class ReportViewModel : ViewModel() {
         }
 
         viewModelScope.launch {
-            val result = repository.addComment(hashMap)
+            when(val result = repository.addComment(hashMap)) {
+
+            }
         }
 
     }
@@ -41,7 +65,9 @@ class ReportViewModel : ViewModel() {
     }
 
     fun deleteReport() {
+        viewModelScope.launch {
 
+        }
     }
 
 }
