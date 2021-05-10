9ace1696da555abae9289289515b4d5d14a8f2c3
minhee <jwmh15@gmail.com>
2021-05-10 23:18:01 +0900

    [Add] Sympathy, Report

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/repository/ReportRepository.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/repository/ReportRepository.kt
@@ -1,11 +1,18 @@
 package com.example.daboyeo_android.repository
 
+import android.database.DataSetObservable
 import com.example.daboyeo_android.base.BaseRepository
+import com.example.daboyeo_android.entity.home.DetailReportData
 import com.example.daboyeo_android.http.interceptor.DaboyeoConnect
 import com.example.daboyeo_android.http.interceptor.Result
+import com.google.gson.JsonObject
 
 class ReportRepository : BaseRepository() {
     private val token = getAccessToken()!!
+
+    suspend fun clickSympathy(reportId: JsonObject) : Result<Boolean> {
+        return mappingToResult { DaboyeoConnect.getAPI().sympathy(token, reportId) }
+    }
 
     suspend fun addComment(hashMap: HashMap<String, String>) : Result<Unit> {
         return mappingToResult { DaboyeoConnect.getAPI().addComment(token, hashMap) }
@@ -17,5 +24,13 @@ class ReportRepository : BaseRepository() {
 
     suspend fun modifyReport(hashMap: HashMap<String, String>) : Result<Unit> {
         return mappingToResult { DaboyeoConnect.getAPI().modifyReport(token, hashMap) }
+    }
+
+    suspend fun deleteReport(reportId: JsonObject) : Result<Unit> {
+        return mappingToResult { DaboyeoConnect.getAPI().deleteReport(token, reportId) }
+    }
+
+    suspend fun getDetailReport(reportId: Int) :Result<DetailReportData> {
+        return mappingToResult { DaboyeoConnect.getAPI().getDetailReport(token,reportId) }
     }
 }
