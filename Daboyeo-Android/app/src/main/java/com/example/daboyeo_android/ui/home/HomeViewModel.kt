f33c01a0bb370e94f80cad022f1f382798dc9c26
minhee <jwmh15@gmail.com>
2021-05-10 22:42:30 +0900

    [Add] getTimeLine

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/home/HomeViewModel.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/home/HomeViewModel.kt
@@ -1,8 +1,42 @@
 package com.example.daboyeo_android.ui.home
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.daboyeo_android.entity.home.ReportData
+import com.example.daboyeo_android.entity.home.ReportListData
 import com.example.daboyeo_android.repository.HomeRepository
+import kotlinx.coroutines.launch
+import com.example.daboyeo_android.http.interceptor.Result
+import com.example.daboyeo_android.ui.home.adapter.ReportsAdapter
 
-class HomeViewModel constructor(val repository: HomeRepository): ViewModel() {
+class HomeViewModel : ViewModel() {
+    private val repository = HomeRepository()
+    private val TAG = "HomeViewModel"
+    val mAdapter = ReportsAdapter()
+
+    private val _reportsData = MutableLiveData<ReportListData>()
+    private val reportsData: LiveData<ReportListData> get() = _reportsData
+
+    init {
+        getTimeLine()
+    }
+
+    private fun getTimeLine() {
+        viewModelScope.launch {
+            when (val result = repository.getTimeLine()) {
+                is Result.Success -> {
+                    _reportsData.value = result.data
+                    mAdapter.setList(reportsData.value!!.reports)
+                }
+                is Result.Error -> {
+                    Log.e(TAG, result.exception)
+                }
+            }
+
+        }
+    }
 
 }
