bdf907b118e7db5b8f7dc9e2c2e12e52ec95d5d4
minhee <jwmh15@gmail.com>
2021-05-12 22:13:42 +0900

    [Add] Toolbar

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/home/HomeActivity.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/home/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.example.daboyeo_android.ui.home
 
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
@@ -11,7 +13,6 @@ import androidx.fragment.app.Fragment
 import com.example.daboyeo_android.R
 import com.example.daboyeo_android.databinding.ActivityHomeBinding
 import com.example.daboyeo_android.ui.profile.ProfileFragment
-import com.example.daboyeo_android.ui.report.DetailPostFragment
 import com.example.daboyeo_android.ui.search.SearchFragment
 import com.example.daboyeo_android.ui.writing.WritingFragment
 
@@ -21,10 +22,10 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
     private val requiredPermissions = arrayOf(
-            android.Manifest.permission.CAMERA,
-            android.Manifest.permission.READ_EXTERNAL_STORAGE,
-            android.Manifest.permission.ACCESS_COARSE_LOCATION,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +33,7 @@ class HomeActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
 
         bottomNavigationItemClick()
-        replaceFragment(HomeFragment())
+        changeView(WritingFragment(),"Writing")
         checkPermissions()
     }
 
@@ -40,23 +41,33 @@ class HomeActivity : AppCompatActivity() {
         var rejectedPermissionList = ArrayList<String>()
 
         for (permission in requiredPermissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
                 rejectedPermissionList.add(permission)
             }
         }
 
         if (rejectedPermissionList.isNotEmpty()) {
             val array = arrayOfNulls<String>(rejectedPermissionList.size)
-            ActivityCompat.requestPermissions(this, rejectedPermissionList.toArray(array), REQUEST_PERMISSIONS)
+            ActivityCompat.requestPermissions(
+                this, rejectedPermissionList.toArray(array), REQUEST_PERMISSIONS
+            )
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        when(requestCode) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
             REQUEST_PERMISSIONS -> {
-                for((i,permission) in permissions.withIndex()) {
-                    if(grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-                        Log.i("HomeActivity","the user has denied to $permission")
+                for ((i, permission) in permissions.withIndex()) {
+                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                        Log.i("HomeActivity", "the user has denied to $permission")
                     }
                 }
             }
@@ -66,29 +77,45 @@ class HomeActivity : AppCompatActivity() {
     private fun bottomNavigationItemClick() {
         binding.homeBottomNavigation.setOnNavigationItemReselectedListener {
             when (it.itemId) {
-                R.id.menu_home_main -> {
-                    replaceFragment(HomeFragment())
-                }
-
-                R.id.menu_home_search -> {
-                    replaceFragment(SearchFragment())
-                }
-
-                R.id.menu_home_writing -> {
-                    replaceFragment(WritingFragment())
-                }
-
-                R.id.menu_home_profile -> {
-                    replaceFragment(ProfileFragment())
-                }
+                R.id.menu_home_main -> changeView(HomeFragment(), "Home")
+                R.id.menu_home_search -> changeView(SearchFragment(), "Search")
+                R.id.menu_home_writing -> changeView(WritingFragment(), "Writing")
+                R.id.menu_home_profile -> changeView(ProfileFragment(), "MyProfile")
             }
         }
     }
 
-    fun replaceFragment(fragment: Fragment) {
+    fun changeView(fragment: Fragment, toolbar: String) {
+        replaceFragment(fragment)
+        setToolbar(toolbar)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.home_fragment, fragment)
         fragmentTransaction.commit()
     }
 
+    private fun setToolbar(fragment: String) {
+        when (fragment) {
+            "Home" -> binding.toolbarTitleTextView.text = getString(R.string.app_name)
+            "Writing" -> {
+                binding.toolbarTitleTextView.text = getString(R.string.writing)
+                binding.toolbarSaveButton.visibility = View.VISIBLE
+                binding.toolbarBackButton.visibility = View.VISIBLE
+            }
+            "MyProfile" -> {
+                binding.toolbarTitleTextView.text = getString(R.string.my_profile)
+                binding.toolbarBackButton.visibility = View.VISIBLE
+                binding.toolbarSettingButton.visibility = View.VISIBLE
+            }
+            "Profile" -> {
+                binding.toolbarTitleTextView.text = getString(R.string.profile)
+                binding.toolbarBackButton.visibility = View.VISIBLE
+            }
+            "Search" -> binding.homeToolbar.visibility = View.GONE
+            "Detail" -> binding.toolbarBackButton.visibility = View.VISIBLE
+
+        }
+    }
 }
