f8a8ea77c6db23c34ae0c29bb080bef33f3c0991
minhee <jwmh15@gmail.com>
2021-05-18 22:41:50 +0900

    [Add] Toolbar

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/home/HomeFragment.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/home/HomeFragment.kt
@@ -1,29 +1,47 @@
 package com.example.daboyeo_android.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.daboyeo_android.R
 import com.example.daboyeo_android.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val viewModel: HomeViewModel by viewModels()
-
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
+        setHasOptionsMenu(true)
+
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.homeToolbar.inflateMenu(R.menu.home_toolbar)
+        binding.homeToolbar.title = getString(R.string.app_name)
+
+        binding.homeToolbar.setOnMenuItemClickListener{
+            when(it.itemId) {
+                R.id.home_toolbar_notifications -> {
+                    Toast.makeText(context, "서비스 준비중입니다.",Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
 }
--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/profile/ProfileFragment.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/profile/ProfileFragment.kt
@@ -6,23 +6,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.example.daboyeo_android.R
 import com.example.daboyeo_android.databinding.FragmentProfileBinding
-import com.example.daboyeo_android.repository.ProfileRepository
-import dagger.hilt.android.AndroidEntryPoint
+import com.example.daboyeo_android.ui.home.HomeActivity
 
-@AndroidEntryPoint
 class ProfileFragment : Fragment() {
-    private lateinit var binding : FragmentProfileBinding
-    lateinit var profileRepo: ProfileRepository
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container,false)
+        val viewModel: ProfileViewModel by viewModels()
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.getMyProfile()
 
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.profileToolbar.inflateMenu(R.menu.profile_toolbar)
+        binding.profileToolbar.title = getString(R.string.my_profile)
+
+        binding.profileToolbar.setOnMenuItemClickListener{
+            when(it.itemId) {
+                R.id.profile_toolbar_modify -> {
+                    (activity as HomeActivity).replaceFragment(ModifyProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 }
