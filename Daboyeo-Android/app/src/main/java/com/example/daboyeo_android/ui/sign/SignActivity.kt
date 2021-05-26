5750e1bfdeff5b164106195bd7390322df64778a
minhee <jwmh15@gmail.com>
2021-05-26 23:07:03 +0900

    [Modify]

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/sign/SignActivity.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/sign/SignActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleOwner
 import com.example.daboyeo_android.R
 import com.example.daboyeo_android.databinding.ActivitySignBinding
 import com.example.daboyeo_android.ui.home.HomeActivity
@@ -21,7 +22,7 @@ class SignActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignBinding
     private lateinit var mGoogleSignInClient: GoogleSignInClient
     private val RC_SIGN_IN = 200
-    private val TAG = "SignActivity"
+    private val TAG = "SignActivity_TAG"
     private val viewModel = SignViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,16 +46,16 @@ class SignActivity : AppCompatActivity() {
             intent()
         }
 
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (GoogleSignIn.getLastSignedInAccount(this) == null) {
-
-        } else {
-            val account = GoogleSignIn.getLastSignedInAccount(this)
-            updateUI(account)
-        }
+        viewModel.signLiveData.observe(this, {
+            when(it){
+                1 -> {
+                    intent()
+                }
+                else-> {
+                    Toast.makeText(this, getString(R.string.unknown_request),Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 
     private fun signIn() {
@@ -73,10 +74,9 @@ class SignActivity : AppCompatActivity() {
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
-            val account = completedTask.getResult(ApiException::class.java)
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
             updateUI(account)
         } catch (e: ApiException) {
-
             Log.w(TAG, "signInResult:failed code=" + e.statusCode)
             updateUI(null)
         }
@@ -84,19 +84,7 @@ class SignActivity : AppCompatActivity() {
 
     private fun updateUI(account: GoogleSignInAccount?) {
         if(account != null) {
-            viewModel.signIn(account.idToken.toString())
-
-            viewModel.signLiveData.observe(this, {
-                if(it == 1) {
-                    intent()
-                } else if(it == 0) {
-                    Toast.makeText(this, "구글 계정을 확인해주세요" ,Toast.LENGTH_LONG).show()
-                } else {
-
-                }
-            })
-        } else {
-
+            viewModel.signIn(account, applicationContext)
         }
     }
 
