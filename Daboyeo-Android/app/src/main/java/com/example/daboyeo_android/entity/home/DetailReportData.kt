fb148e3aef3fe8176286ff93af455f804612c71f
minhee <jwmh15@gmail.com>
2021-05-26 22:59:21 +0900

    [Delete] SerializedName

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/entity/home/DetailReportData.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/entity/home/DetailReportData.kt
@@ -1,24 +1,24 @@
 package com.example.daboyeo_android.entity.home
 
-import com.google.gson.annotations.SerializedName
-
 data class DetailReportData (
-        @SerializedName("report_id") val report_id: String,
-        @SerializedName("reporter_name")  val reporter_name: String,
-        @SerializedName("reporter_profile_uri") val reporter_profile_uri: String,
-        @SerializedName("content") val content: String,
-        @SerializedName("tags") val tags: List<String>,
-        @SerializedName("image_uris") val image_uris: List<String>,
-        @SerializedName("comments") val commentData: List<CommentData>,
-        @SerializedName("num_of_sympathy") val num_of_sympathy : Int,
-        @SerializedName("is_sympathy") val is_sympathy: Boolean,
-        @SerializedName("created_at") val created_at: String,
-        @SerializedName("updated_at") val updated_at: String,
-        @SerializedName("location") val location: String
+        val report_id: String,
+        val reporter_name: String,
+        val reporter_profile_uri: String,
+        val content: String,
+        val tags: List<String>,
+        val image_uris: List<String>,
+        val commentData: List<CommentData>,
+        val num_of_sympathy : Int,
+        val is_sympathy: Boolean,
+        val created_at: String,
+        val updated_at: String,
+        val location: String
         )
 
 data class CommentData(
-        @SerializedName("profile_uri") val profile_uri: String,
-        @SerializedName("name") val name: String,
-        @SerializedName("content") val content: String
+        val comment_id: Int,
+        val profile_uri: String,
+        val name: String,
+        val content: String,
+        val user_id: Int
         )
