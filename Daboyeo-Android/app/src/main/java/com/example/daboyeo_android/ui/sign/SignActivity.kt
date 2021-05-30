0fef1138b30d4bc615bbcc45e6d3a8bbba38b16b
minhee <jwmh15@gmail.com>
2021-05-30 23:55:04 +0900

    [Fix] get access Token

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/sign/SignActivity.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/sign/SignActivity.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LifecycleOwner
 import com.example.daboyeo_android.R
 import com.example.daboyeo_android.databinding.ActivitySignBinding
 import com.example.daboyeo_android.ui.home.HomeActivity
@@ -14,9 +13,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.Scopes
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
 import com.google.android.gms.tasks.Task
+import okhttp3.*
+import org.json.JSONException
+import org.json.JSONObject
 
 class SignActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignBinding
@@ -24,6 +28,7 @@ class SignActivity : AppCompatActivity() {
     private val RC_SIGN_IN = 200
     private val TAG = "SignActivity_TAG"
     private val viewModel = SignViewModel()
+    private var clientSecret = "Pq6vpyaO1dSMZxnGmnJVtApz"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +37,11 @@ class SignActivity : AppCompatActivity() {
         binding.signLoginButton.setSize(SignInButton.SIZE_STANDARD)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .build()
+                .requestEmail()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestScopes(Scope(Scopes.DRIVE_APPFOLDER))
+                .requestServerAuthCode(getString(R.string.default_web_client_id))
+                .build()
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
@@ -47,12 +54,12 @@ class SignActivity : AppCompatActivity() {
         }
 
         viewModel.signLiveData.observe(this, {
-            when(it){
+            when (it) {
                 1 -> {
                     intent()
                 }
-                else-> {
-                    Toast.makeText(this, getString(R.string.unknown_request),Toast.LENGTH_SHORT).show()
+                else -> {
+                    Toast.makeText(this, getString(R.string.unknown_request), Toast.LENGTH_SHORT).show()
                 }
             }
         })
@@ -75,16 +82,49 @@ class SignActivity : AppCompatActivity() {
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
-            updateUI(account)
+            val authCode = account?.serverAuthCode
+
+            val client = OkHttpClient()
+            val requestBody: RequestBody = FormBody.Builder()
+                    .add("grant_type", "authorization_code")
+                    .add("client_id", getString(R.string.default_web_client_id))
+                    .add("client_secret", clientSecret)
+                    .add("redirect_uri", "http://127.0.0.1")
+                    .add("code", authCode.toString())
+                    .add("id_token", account?.idToken.toString())
+                    .build()
+
+            val request: Request = Request.Builder()
+                    .url("https://www.googleapis.com/oauth2/v4/token")
+                    .post(requestBody)
+                    .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: java.io.IOException) {
+                    Log.e(TAG, "onFailure : $e")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    try {
+                        Log.e(TAG, "onResponse : ${response.code}")
+
+                        val jsonObject = JSONObject(response.body?.string())
+                        val message: String = jsonObject.toString(5)
+                        var accessToken = jsonObject.getString("access_token")
+
+                        Log.e(TAG, message)
+                        runOnUiThread {
+                            viewModel.login(accessToken)
+                        }
+
+                    } catch (e: JSONException) {
+                        Log.e(TAG, e.toString())
+                        e.printStackTrace()
+                    }
+                }
+            })
         } catch (e: ApiException) {
             Log.w(TAG, "signInResult:failed code=" + e.statusCode)
-            updateUI(null)
-        }
-    }
-
-    private fun updateUI(account: GoogleSignInAccount?) {
-        if(account != null) {
-            viewModel.signIn(account, applicationContext)
         }
     }
 
@@ -92,5 +132,4 @@ class SignActivity : AppCompatActivity() {
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
     }
-
 }
--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/sign/SignViewModel.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/ui/sign/SignViewModel.kt
@@ -1,40 +1,24 @@
 package com.example.daboyeo_android.ui.sign
 
-import android.content.Context
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.daboyeo_android.repository.SignRepository
-import kotlinx.coroutines.launch
-import com.example.daboyeo_android.http.interceptor.Result
-import com.google.android.gms.auth.GoogleAuthUtil
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.common.GooglePlayServicesUtil
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.collections.HashMap
+import com.example.daboyeo_android.http.interceptor.Result
 
 class SignViewModel : ViewModel() {
     private val repository = SignRepository()
     var signLiveData = MutableLiveData<Int>()
-    private val scope = "audience:server:client_id:579492909337-40q0b10l9bidbcdofmm1uggi95clbbn0.apps.googleusercontent.com"
 
-    private fun getAccessToken(account: GoogleSignInAccount, applicationContext: Context){
-        viewModelScope.launch(Dispatchers.IO) {
-            getAccessToken = GoogleAuthUtil.getToken(applicationContext, account.account, scope)
-            Log.e("SignViewModel", "token in scope : $getAccessToken")
-        }
-    }
-
-    fun signIn(account: GoogleSignInAccount, applicationContext: Context) {
-        //getAccessToken(account, applicationContext)
+    fun login(accessToken: String) {
         var hashMap = HashMap<String, String>()
 
-        if(getAccessToken != null) {
-            hashMap["token"] = getAccessToken
-            hashMap["auth_type"] = "google"
-        } else {
-            Log.e("SignViewModel", "getAccessToken is null")
-        }
+        hashMap["token"] = accessToken
+        hashMap["auth_type"] = "google"
 
         viewModelScope.launch {
             when (val result = repository.signAuth(hashMap)) {
@@ -51,4 +35,5 @@ class SignViewModel : ViewModel() {
             }
         }
     }
+
 }
