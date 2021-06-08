88aa104165b4438e015f51becd903af71d8dedb7
minhee <jwmh15@gmail.com>
2021-06-08 22:32:51 +0900

    [Add] Get Google Access Token

--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/interceptor/DaboyeoConnect.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/interceptor/DaboyeoConnect.kt
@@ -2,9 +2,6 @@ package com.example.daboyeo_android.http.interceptor
 
 import com.example.daboyeo_android.http.service.DaboyeoService
 import com.example.daboyeo_android.http.service.FileService
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -13,10 +10,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 object DaboyeoConnect {
     private var retrofit: Retrofit
     private var fileRetrofit: Retrofit
+    private var tokenRetrofit: Retrofit
     private var api: DaboyeoService
     private var fileApi : FileService
+    private var tokenApi : DaboyeoService
     private const val baseURL = "https://6l8nidp3k7.execute-api.ap-northeast-2.amazonaws.com/dev/"
     private const val fileServer = "http://54.180.105.66:8000/"
+    private const val googleTokenServer = "https://www.googleapis.com/"
 
     init {
         val loggingInterceptor = HttpLoggingInterceptor()
@@ -38,11 +38,21 @@ object DaboyeoConnect {
             .client(client)
             .build()
 
+         tokenRetrofit = Retrofit.Builder()
+             .baseUrl(googleTokenServer)
+             .addConverterFactory(GsonConverterFactory.create())
+             .client(client)
+             .build()
+
         api = retrofit.create(DaboyeoService::class.java)
         fileApi = fileRetrofit.create(FileService::class.java)
+        tokenApi = tokenRetrofit.create(DaboyeoService::class.java)
+
     }
 
     fun getAPI() = api
 
     fun getFileAPI() = fileApi
+
+    fun getGoogleToken() = tokenApi
 }
--- Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/service/SignService.kt
+++ Daboyeo-Android/app/src/main/java/com/example/daboyeo_android/http/service/SignService.kt
@@ -1,5 +1,7 @@
 package com.example.daboyeo_android.http.service
 
+import com.example.daboyeo_android.entity.sign.GoogleTokenRequest
+import com.example.daboyeo_android.entity.sign.GoogleTokenResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -9,4 +11,10 @@ interface SignService {
     suspend fun signAuth(
         @Body body: HashMap<String, String>
     ): Response<String>
+
+    @POST("oauth2/v4/token")
+    suspend fun getAccessToken(
+        @Body body: GoogleTokenRequest
+    ): Response<GoogleTokenResponse>
+
 } 
